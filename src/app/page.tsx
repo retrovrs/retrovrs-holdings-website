eb18b75721f183f65cbfb507f6d6dfe187475361
Solène <81546747+solenedel@users.noreply.github.com>
2025-04-21 16:04:16 -0700

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Bebas_Neue } from 'next/font/google';
+import styles from '../../styles/Button.module.css';
 
 const bebasNeue = Bebas_Neue({
   weight: '400',
@@ -10,8 +11,9 @@ export default function Home() {
   return (
     <div className=" min-h-screen w-full pb-20 bg-fixed">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <header className="bg-[url('/images/banner.png')] bg-contain flex flex-col justify-center items-center bg-center bg-no-repeat w-full h-[550px] ">
-          <h1 className="z-10 self-center relative bottom-16 opacity-80">
+        <header className="relative flex flex-col justify-center items-center w-full h-[550px]">
+          <div className="absolute inset-0 bg-[url('/images/banner2.png')] bg-contain bg-center bg-no-repeat opacity-50 z-0"></div>
+          <h1 className="z-10 self-center relative bottom-16 ">
             <Image
               src="/images/holdings_logo.png"
               alt="logo"
@@ -19,32 +21,120 @@ export default function Home() {
               height={100}
             />
           </h1>
+          <p className="italic z-10 self-center relative  text-lg tracking-wide opacity-95 font-semibold p-1">
+            We build B2C and B2B web3 products for the luxury resale market.
+          </p>
         </header>
-        <p className="z-10 self-center relative bottom-16 text-xl">
-          We build B2C and B2B web3 products for the luxury resale market.
-        </p>
+
         <div className="flex flex-col text-center justify-center items-center w-full">
+          <em className="block mt-4 opacity-90 uppercase tracking-widest font-light animate-bounce">
+            Now in Beta{' '}
+            <span className="inline-block transform rotate-180 ">⇪</span>
+          </em>
+
           <h2
-            className={`${bebasNeue.className} text-3xl tracking-wider mb-10 text-indigo-300 `}>
-            In development
+            className={`${bebasNeue.className} text-3xl tracking-wider mt-5 mb-12 `}>
+            <span
+              style={{
+                backgroundImage:
+                  'radial-gradient(circle at center, #6597f6, #c587ff)',
+                WebkitBackgroundClip: 'text',
+                backgroundClip: 'text',
+                color: 'transparent',
+                display: 'block',
+              }}
+              className="text-4xl font-bold tracking-widest">
+              RETRO//VRS marketplace
+            </span>
           </h2>
-          <section className="grid grid-cols-2 gap-32 items-between ">
-            <div className="relative w-[300px] h-[200px]">
-              <video
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="w-full h-full object-cover rounded-lg shadow-lg">
-                <source src="/video/retrovrs-demo.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              <p
-                className={`${bebasNeue.className} text-xl tracking-wider mt-6 `}>
-                RETRO//VRS MARKETPLACE
+          <aside className="flex gap-4 items-center justify-center w-full mb-8">
+            <Image
+              src="/images/retrovrs.png"
+              className="self-center rounded-md"
+              alt="logo"
+              width={500}
+              height={100}
+            />
+          </aside>
+          <p className="text-md mt-2 tracking-wide opacity-90 w-[60%]">
+            A B2C resale marketplace for luxury goods, where each item is
+            authenticated before posting and has its own PBT (Physically Backed
+            Token). Built on the Polygon blockchain network, with built-in
+            lifetime royalties when you re-sell your item on the platform in the
+            future. Now in curated beta, accepting bags and watches.
+          </p>
+          <button
+            className={`${bebasNeue.className}  hover:cursor-pointer relative border text-indigo-200 border-indigo-400 text-2xl flex items-center justify-center tracking-wider bg-black px-4 pt-2 pb-1 rounded-md mt-16 ${styles.glowButton}`}>
+            <a href="https://retrovrs.com" target="_blank">
+              Explore the marketplace
+            </a>
+            <span className="inline-block transform rotate-90 text-2xl pb-4">
+              ⇪
+            </span>
+          </button>
+
+          <section className="grid grid-cols-2 items-between mt-36 ">
+            <div className="flex flex-col gap-4 items-center justify-center">
+              <h2
+                className={`${bebasNeue.className} text-3xl tracking-wider mt-5 mb-5 `}>
+                <span
+                  style={{
+                    backgroundImage:
+                      'radial-gradient(circle at center, #6597f6, #c587ff)',
+                    WebkitBackgroundClip: 'text',
+                    backgroundClip: 'text',
+                    color: 'transparent',
+                    display: 'block',
+                  }}
+                  className="text-4xl font-bold tracking-widest mb-5">
+                  LUXE\\LINKS
+                </span>
+                <Image
+                  src="/images/luxe-links.png"
+                  alt="retrovrs logo"
+                  width={200}
+                  height={200}
+                />
+              </h2>
+              <p className="text-md tracking-wide opacity-90 w-3/5">
+                A B2B SaaS creating Digital Provenance Passports (DPvP) with
+                built in royalty attribution for existing marketplaces, brands,
+                and boutiques that sell pre-owned luxury assets. Each asset is
+                tokenized using our native LUXE\\LINKS token.
               </p>
             </div>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col gap-4 items-center justify-center">
+              <h2
+                className={`${bebasNeue.className} text-3xl tracking-wider mt-5 mb-5 `}>
+                <span
+                  style={{
+                    backgroundImage:
+                      'radial-gradient(circle at center, #6597f6, #c587ff)',
+                    WebkitBackgroundClip: 'text',
+                    backgroundClip: 'text',
+                    color: 'transparent',
+                    display: 'block',
+                  }}
+                  className="text-4xl font-bold tracking-widest mb-5">
+                  RETRO//VAULT
+                </span>
+                <Image
+                  src="/images/retrovault.png"
+                  alt="retrovrs logo"
+                  width={200}
+                  height={200}
+                />
+              </h2>
+              <p className="text-md tracking-wide opacity-90 w-3/5">
+                A B2C and B2B authentication service utilizing computer vision
+                AI, machine learning, deep learning, and blockchain to verify,
+                twin, and tokenize luxury assets, offering Digital Provenance
+                Passports (DPvP) and secure smart contracts for enhanced trust
+                and transparency.
+              </p>
+            </div>
+
+            {/* <div className="flex flex-col items-center">
               <Image
                 src="/images/luxe-links.png"
                 alt="retrovrs logo"
@@ -55,7 +145,7 @@ export default function Home() {
                 className={`${bebasNeue.className} text-xl tracking-wider mt-6 `}>
                 LUXE\\LINKS
               </p>
-            </div>
+            </div> */}
           </section>
         </div>
       </main>
---
+++ tailwind.config.js
@@ -0,0 +1,104 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mainFont: ['IBM Plex Mono', 'sans-serif'],
+        numberFont: ['Kode mono', 'sans-serif'],
+        auroraTitle: ['ZCOOL QingKe HuangYou', 'sans-serif'],
+        auroraSubheading: ['Geo', 'sans-serif'],
+        kanjiFont: ['Noto Serif JP', 'serif'],
+        bebas: ['Bebas Neue', 'sans-serif'],
+      },
+      backgroundImage: {},
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+          },
+          '50%': {
+            opacity: '0.5',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
+        'fade-out-up': {
+          '0%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            opacity: '0.5',
+            transform: 'translateY(0)',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+        },
+        fadeIn: {
+          '0%': { opacity: '0%' },
+          '50%': { opacity: '70%' },
+          '100%': { opacity: '100%' },
+        },
+        fadeSlow: {
+          '0%': { opacity: '0%' },
+          '50%': { opacity: '0%' },
+          '75%': { opacity: '50%' },
+          '100%': { opacity: '100%' },
+        },
+        shadowChange: {
+          '0%': { boxShadow: '0 0 30px rgba(37,99,235, 1)' },
+          '50%': { boxShadow: '0 0 30px rgba(37,99,235, 0.3)' },
+          '100%': { boxShadow: '0 0 30px rgba(37,99,235, 1)' },
+        },
+        violetGlow: {
+          '0%': { boxShadow: '0 0 30px rgba(124, 58, 237,0.3)' },
+          '50%': { boxShadow: '0 0 30px rgba(124, 58, 237, 1)' },
+          '100%': { boxShadow: '0 0 30px rgba(124, 58, 237, 0.3)' },
+        },
+        colorFade: {
+          '0%': { backgroundColor: 'rgba(251, 146, 60, 1)' },
+          '100%': { backgroundColor: 'rgba(3, 7, 18, 1)' },
+        },
+        orangeButton: {
+          '0%': { backgroundColor: 'rgba(99,53,15, 0.5)' },
+          '50%': { backgroundColor: 'rgba(99,53,15, 0.2)' },
+          '100%': { backgroundColor: 'rgba(99,53,15, 0.5)' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in-down 0.5s ease-in',
+        'fade-out': 'fade-out-up 0.5s ease-out',
+        fade: 'fadeIn 1s ease-in',
+        fadeInSlow: 'fadeSlow 3s ease-in',
+        shadowChange: 'shadowChange 2.5s infinite',
+        oneSecond: 'violetGlow 1s infinite',
+        colorFade: 'colorFade 2s ease-in-out forwards',
+        buttonFlash: 'orangeButton 3s ease-in-out infinite',
+      },
+      boxShadow: {
+        glow: '0 0 10px rgba(37,99,235,0.3), 0 0 10px rgba(37,99,235,0.3), 0 0 10px rgba(37,99,235,0.3), 0 0 10px rgba(37,99,235,0.3)',
+        purpleGlow:
+          '0 0 20px rgba(79,70,229,0.2), 0 0 20px rgba(79,70,229,0.2), 0 0 20px rgba(79,70,229,0.2), 0 0 20px rgba(79,70,229,0.2)',
+        footer: '0 -20px 40px rgba(37,99,235,0.4)',
+        rainbow:
+          '0 0 10px rgba(139, 92, 246,0.3), 0 0 20px rgba(139, 92, 246,0.3), 0 0 10px rgba(56, 189, 248,0.3), 0 0 10px rgba(56, 189, 248,0.3)',
+        violetGlow:
+          '0 0 20px rgba(124, 58, 237,0.2), 0 0 20px rgba(124, 58, 237,0.2), 0 0 20px rgba(124, 58, 237,0.2), 0 0 20px rgba(124, 58, 237,0.2)',
+      },
+      cursor: {
+        custom: 'url("/cursor.png"), auto',
+        'pointer-custom': 'url("/link-cursor.png"), pointer',
+      },
+    },
+  },
+  plugins: [],
+};
